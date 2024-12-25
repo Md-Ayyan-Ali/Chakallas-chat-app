f25c5d7130a2dbec9a6dcb8112214b5a6bc8dd72
Md Ayyan Ali <ayyanali0809@gmail.com>
2024-12-25 21:28:10 +0530

    4th part completed-socket

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
-import Conversation from './Conversation.jsx'
+import React from "react";
+import useGetConversations from "../../hooks/useGetConversations.js";
+import Conversation from "./Conversation.jsx";
+import { getRandomEmoji } from "../../utils/emojis.js";
 
 const Conversations = () => {
-  return (
-    <div className='py-2 flex flex-col overflow-auto'>
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-    </div>
-  )
-}
+	const { loading, conversations } = useGetConversations();
+  console.log("CONVERSATIONS:", conversations);
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
 
+      {conversations.map((conversation, idx) => (
+        <Conversation 
+        key = {conversation._id}
+        conversation = {conversation} 
+        emoji={getRandomEmoji()}
+        lastIdx = {idx === conversations.length - 1}
+        />
+      ))}
+				
+      {loading ? <span className="loading loading-spinner mx-auto"></span> : null}
+		</div>
+	);
+};
 export default Conversations;
+
+
+// import React from 'react'
+// import Conversation from './Conversation.jsx'
+// import useGetConversations from '../../hooks/useGetConversations.js'
+
+// const Conversations = () => {
+//  const { loading, conversations } = useGetConversations();
+//  console.log("CONVERSATIONS", conversations);
+//   return (
+//     <div className='py-2 flex flex-col overflow-auto'>
+//         <Conversation />
+//         <Conversation />
+//         <Conversation />
+//         <Conversation />
+//         <Conversation />
+//         <Conversation />
+//     </div>
+//   )
+// }
+
+// export default Conversations;
 
 
 
